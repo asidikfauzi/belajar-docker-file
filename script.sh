6509f57edda8a03cf53726cb74d1c31a91f9dbd8
asidikfauzi <asidikfauzi@gmail.com>
2022-12-08 17:01:19 +0700

    run

--- script.sh
+++ script.sh
@@ -1,3 +1,11 @@
+# FROM Instruction
 docker build -t asidikfauzi/from from
-
 docker image ls
+
+#RUN Instruction
+docker build -t asidikfauzi/run run
+#jika run tidak ingin menggunakan cache
+docker build -t asidikfauzi/run run --progress=plain --no-cache
+
+#cara melihat docker image dengan kata uniq
+docker image ls | asidikfauzi

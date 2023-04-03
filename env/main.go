13348aee4a0864a5d732cb3c6f317aeba8126644
asidikfauzi <asidikfauzi@gmail.com>
2023-04-03 15:12:54 +0700

    environtment variable instruction

--- env/main.go
+++ env/main.go
@@ -1,1 +1,18 @@
-package env
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("APP_PORT")
+	fmt.Println("Run app in port : " + port)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}

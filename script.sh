cdd0ccefc27453550a81b6e585bd1a0137d5a1fe
asidikfauzi <asidikfauzi@gmail.com>
2023-04-04 14:25:20 +0700

    script sh

--- script.sh
+++ script.sh
@@ -53,3 +53,118 @@ docker container create --name ignore asidikfauzi/ignore
 docker container start ignore
 
 docker container logs ignore
+
+
+#EXPOSE instruction
+docker build -t udemy/expose expose
+
+docker image inspect udemy/expose
+
+docker container create --name expose -p 8080:8080 udemy/expose
+
+docker container start expose
+
+docker container logs expose
+
+#ENV instruction
+docker build -t udemy/env env
+
+docker image inspect udemy/env
+
+docker container create --name env --env APP_PORT=9090 -p 9090:9090 udemy/env
+
+docker container start env
+
+docker container logs env
+
+docker container stop env
+
+#Volume Instruction
+docker build -t udemy/volume volume
+
+docker image inspect udemy/volume
+
+docker container create --name volume -p 8080:8080 udemy/volume
+
+docker container start volume
+
+docker container logs volume
+
+docker container inspect volume
+#f302145ce820f079bcea044e0f7eee91325a8db42f472725baa12c127435ce2f
+
+docker container stop volume
+
+#WORKDIR Instruction
+docker build -t udemy/workdir workdir
+
+docker image inspect udemy/workdir
+
+docker container create --name workdir -p 8080:8080 udemy/workdir
+
+docker container start workdir
+
+docker container exec -it workdir /bin/sh
+
+
+#USER Instruction
+docker build -t udemy/user user
+
+docker image inspect udemy/user
+
+docker container create --name user -p 8080:8080 udemy/user
+
+docker container start user
+
+docker container exec -it user /bin/sh
+
+#argument Instruction
+docker build -t udemy/arg arg --build-arg app=pzn
+
+docker image inspect udemy/arg
+
+docker container create --name arg -p 8080:8080 udemy/arg
+
+docker container start arg
+
+docker container exec -it arg /bin/sh
+
+#health Instruction
+docker build -t udemy/health health
+
+docker image inspect udemy/health
+
+docker container create --name health -p 8080:8080 udemy/health
+
+docker container start health
+
+docker ps
+
+docker ps | grep unhealthy
+
+docker container inspect health
+
+
+#entrypoint Instruction
+docker build -t udemy/entrypoint entrypoint
+
+docker image inspect udemy/entrypoint
+
+docker container create --name entrypoint -p 8080:8080 udemy/entrypoint
+
+docker container start entrypoint
+
+docker container inspect entrypoint
+
+
+
+#Multi stage Instruction
+docker build -t asidikfauzi/multi multi
+
+docker image ls
+
+docker container create --name multi -p 8080:8080 asidikfauzi/multi
+
+docker container start multi
+
+docker container inspect multi
